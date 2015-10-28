bdd1602006d224246d70322c2095fc7b81d88166
enleightond <enleightond@yahoo.com>
2015-10-28 15:47:27 -0700

    complete virtual cat

--- virtualcat.js
+++ virtualcat.js
@@ -1,41 +1,52 @@
-var cat = function (firstName, tiredness, hungry, loneliness, happiness) {
-	this.firstName = firstName
-	this.tiredness = tiredness 
-	this.hungry = hungry
-	this.loneliness = lonliness
-	this.happiness = happiness
+var cat = function (f, t, h, l, ha) {
+	this.firstName = f
+	this.tiredness = t
+	this.hungry = h
+	this.loneliness = l
+	this.happiness = ha
 }
-cat.prototype.firstName = function(firstName)
 	
 cat.prototype.sleep = function(tiredness) {
-  	if (tiredness.status = 0) {
-  		console.log("sleepytime")		
-	} else {
+  	if (this.tiredness <= 4) {
+  		console.log("sleepytime")
+  		this.tiredness++
+	} else if (this.tiredness >=5) {
 		console.log("playtime!")
+	} else {
+		console.log("MEow!")
 	}
   }
 cat.prototype.feed = function(hungry) {
-	if (tiredness.status = 0) {
-  		console.log("dinnertime")		
-	} else {
+	if (this.hungry <= 4) {
+  		console.log("dinnertime")
+  		this.hungry++		
+	} else if (this.hungry >=5) {
 		console.log("playtime!")
+	} else {
+		console.log("MEow!")
 	}
   }
 cat.prototype.pet = function(loneliness) {
-	if (tiredness.status = 0) {
-  		console.log("cuddletime")		
-	} else {
+	if (this.loneliness <= 4) {
+  		console.log("cuddletime")
+  		this.loneliness++		
+	} else if (this.loneliness >=5) {
 		console.log("playtime!")
+	} else {
+		console.log("MEow!")
 	}
   }
 cat.prototype.purr = function(happiness) {
-	if (tiredness.status = 0) {
-  		console.log("playtime")		
-	} else {
+	if (this.happiness <= 2) {
+  		console.log("playtime")
+  		this.happiness++		
+	} else if (this.happiness >=3){
 		console.log("alooftime!")
+	} else {
+		console.log("MEow!")
 	}
   }
-var loki = new cat ("Loki" 1,0,1,0,)
+var loki = new cat ("Loki", 1,0,1,0)
 
 console.log(cat);
 
